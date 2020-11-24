9e2fef615652680a030ee838a0fd024f87722b1e
SheHuan <she320955huan>
2020-11-24 23:21:10 +0800

    fix

--- src/main/java/com/sn/elasticsearch/controller/IndexController.java
+++ src/main/java/com/sn/elasticsearch/controller/IndexController.java
@@ -5,8 +5,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-    @GetMapping({"/", "/index"})
-    public String index() {
-        return "index";
-    }
+
 }
